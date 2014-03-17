1a5abd5aed1e6389663f3f641517f4a48b7c38ac
Guowei Zhao <zhaoguowei@foxmail.com>
2014-03-17 16:27:35 +0800

    供应商表添加税号字段
    - 在suppliers表中添加taxid字段，保存纳税人识别号(税号)字段。并修改相应页面。
    - 整理数据库，上传最新数据结构文件。

--- db/cmis.sql
+++ db/cmis.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 03 月 15 日 17:34
+-- 生成日期: 2014 年 03 月 17 日 08:24
 -- 服务器版本: 5.5.28
 -- PHP 版本: 5.4.24
 
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- 表的结构 `assets`
 --
 
-CREATE TABLE IF NOT EXISTS `assets` (
+CREATE TABLE `assets` (
   `aID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `dID` int(10) unsigned NOT NULL COMMENT '出入库ID',
   `assetno` varchar(10) DEFAULT NULL COMMENT '固定资产统一编号',
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `assets` (
   `departno` varchar(50) DEFAULT NULL COMMENT '电教资产编号',
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`aID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='固定资产表' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='固定资产表';
 
 -- --------------------------------------------------------
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `assets` (
 -- 表的结构 `checks`
 --
 
-CREATE TABLE IF NOT EXISTS `checks` (
+CREATE TABLE `checks` (
   `cID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `checkno` varchar(10) NOT NULL COMMENT '支票号',
   `holder` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '支付账户',
@@ -55,14 +55,7 @@ CREATE TABLE IF NOT EXISTS `checks` (
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`cID`),
   UNIQUE KEY `checkno_UNIQUE` (`checkno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- 转存表中的数据 `checks`
---
-
-INSERT INTO `checks` (`cID`, `checkno`, `holder`, `invoicedate`, `purpose`, `money`, `sID`, `duedate`, `receiptor`, `memo`) VALUES
-(1, '03485873', 0, '2014-03-13', '购买投影机灯泡', '1300.00', 5, NULL, NULL, '政采');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -70,7 +63,7 @@ INSERT INTO `checks` (`cID`, `checkno`, `holder`, `invoicedate`, `purpose`, `mon
 -- 表的结构 `depots`
 --
 
-CREATE TABLE IF NOT EXISTS `depots` (
+CREATE TABLE `depots` (
   `dID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `iID` int(10) unsigned NOT NULL COMMENT '货物ID',
   `operate` varchar(3) NOT NULL DEFAULT 'XG0' COMMENT '出入库指令',
@@ -81,30 +74,7 @@ CREATE TABLE IF NOT EXISTS `depots` (
   `detail` varchar(45) NOT NULL COMMENT '使用详细说明，新购时填写对应发票ID',
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`dID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='出入库表' AUTO_INCREMENT=23 ;
-
---
--- 转存表中的数据 `depots`
---
-
-INSERT INTO `depots` (`dID`, `iID`, `operate`, `amount`, `price`, `datetime`, `mID`, `detail`, `memo`) VALUES
-(1, 6, 'XG1', 10, NULL, '2014-03-13 00:00:00', 1, '打印机耗材更换预留', '1月预留'),
-(2, 6, 'SY0', 3, NULL, '2014-03-13 08:31:52', 3, '教务处更换', '教务处3台打印机更换耗材'),
-(3, 6, 'SY0', 1, NULL, '2014-03-13 08:34:21', 3, '招生办更换', ''),
-(4, 6, 'JH0', 2, NULL, '2014-03-13 08:36:36', 1, '长时间未使用报废', '因未即使使用导致2个硒鼓过期，不能使用。'),
-(5, 6, 'SY1', 1, NULL, '2014-03-13 16:38:02', 3, '教务处有一个未使用', '教务处只更换了2个'),
-(6, 6, 'SY0', 3, NULL, '2014-03-13 16:40:59', 3, '校办', ''),
-(7, 9, 'XG1', 10, NULL, '2014-03-11 17:16:01', 1, '备用耗材', ''),
-(8, 9, 'SY0', 2, NULL, '2014-03-13 17:18:00', 1, 'AP更换', 'AP111'),
-(9, 8, 'XG1', 1, NULL, '2014-03-11 22:13:52', 4, '图书馆购买', ''),
-(10, 8, 'SY0', 1, NULL, '2014-03-13 22:20:18', 5, '副校长', ''),
-(16, 7, 'XG1', 2, NULL, '2014-03-12 22:46:56', 11, '图书馆购', ''),
-(17, 8, 'JH0', 1, NULL, '2014-03-13 23:08:41', 12, '大港五中', ''),
-(18, 8, 'JH1', 1, NULL, '2014-03-13 23:14:06', 13, '大港五中还', ''),
-(19, 7, 'JH0', 1, NULL, '2014-03-11 23:29:23', 12, '教务处借', ''),
-(20, 7, 'BF0', 1, NULL, '2014-03-13 23:38:40', 12, '损坏报废', ''),
-(21, 6, 'XG1', 5, NULL, '2014-03-14 14:00:06', 5, '备用', ''),
-(22, 6, 'SY0', 2, NULL, '2014-03-14 22:37:29', 3, '全球万恶请问', '');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='出入库表';
 
 -- --------------------------------------------------------
 
@@ -112,18 +82,19 @@ INSERT INTO `depots` (`dID`, `iID`, `operate`, `amount`, `price`, `datetime`, `m
 -- 表的结构 `invoices`
 --
 
-CREATE TABLE IF NOT EXISTS `invoices` (
+CREATE TABLE `invoices` (
   `vID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `invoiceno` varchar(15) NOT NULL COMMENT '发票号',
   `holder` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '支付来源',
   `invoicedate` date NOT NULL COMMENT '发票日期',
   `sID` mediumint(8) unsigned NOT NULL COMMENT '供应商ID',
   `items` text NOT NULL COMMENT '货物明细 出入库ID',
+  `money` decimal(9,2) unsigned NOT NULL COMMENT '发票金额',
   `projectno` varchar(25) DEFAULT NULL COMMENT '政采计划编号',
   `cID` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '支票号',
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`vID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -131,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `invoices` (
 -- 表的结构 `items`
 --
 
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE `items` (
   `iID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL COMMENT '货物名称',
   `brand` varchar(20) DEFAULT NULL COMMENT '货物品牌',
@@ -141,17 +112,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `tID` tinyint(3) unsigned DEFAULT NULL COMMENT '货物类别ID',
   `memo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`iID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='货物表' AUTO_INCREMENT=15 ;
-
---
--- 转存表中的数据 `items`
---
-
-INSERT INTO `items` (`iID`, `name`, `brand`, `type`, `unit`, `amount`, `tID`, `memo`) VALUES
-(6, '硒鼓', 'HP', '88A', '个', 5, 1, '打印机原装硒鼓'),
-(7, '打印机', '惠普', 'P1108', '台', 0, 2, '办公用打印机配件88A'),
-(8, '计算机', '戴尔', 'P5Z1', '台', 1, 2, '戴尔台式机'),
-(9, '硒鼓', 'HP', '128A', '个', 8, 1, 'HP 1525n 耗材');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='货物表';
 
 -- --------------------------------------------------------
 
@@ -159,20 +120,11 @@ INSERT INTO `items` (`iID`, `name`, `brand`, `type`, `unit`, `amount`, `tID`, `m
 -- 表的结构 `itemtype`
 --
 
-CREATE TABLE IF NOT EXISTS `itemtype` (
+CREATE TABLE `itemtype` (
   `tID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(15) NOT NULL COMMENT '货物类别名称',
   PRIMARY KEY (`tID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='货物类别表' AUTO_INCREMENT=4 ;
-
---
--- 转存表中的数据 `itemtype`
---
-
-INSERT INTO `itemtype` (`tID`, `name`) VALUES
-(1, '办公消耗品'),
-(2, '电脑设备'),
-(3, '网络设备');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='货物类别表';
 
 -- --------------------------------------------------------
 
@@ -180,7 +132,7 @@ INSERT INTO `itemtype` (`tID`, `name`) VALUES
 -- 表的结构 `logs`
 --
 
-CREATE TABLE IF NOT EXISTS `logs` (
+CREATE TABLE `logs` (
   `lID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uid` tinyint(3) unsigned NOT NULL COMMENT '用户ID',
   `table` varchar(15) NOT NULL COMMENT '表名',
@@ -188,32 +140,7 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `opID` int(10) unsigned NOT NULL COMMENT '操作ID',
   `time` datetime NOT NULL COMMENT '操作时间',
   PRIMARY KEY (`lID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='日志表' AUTO_INCREMENT=20 ;
-
---
--- 转存表中的数据 `logs`
---
-
-INSERT INTO `logs` (`lID`, `uid`, `table`, `op`, `opID`, `time`) VALUES
-(1, 1, 'items', 'PUT', 7, '2014-03-13 23:38:40'),
-(2, 1, 'depots', 'NEW', 20, '2014-03-13 23:38:40'),
-(3, 1, 'items', 'PUT', 6, '2014-03-14 14:00:06'),
-(4, 1, 'depots', 'NEW', 21, '2014-03-14 14:00:06'),
-(5, 1, 'items', 'PUT', 6, '2014-03-14 22:37:29'),
-(6, 1, 'depots', 'NEW', 22, '2014-03-14 22:37:29'),
-(7, 1, 'suppliers', 'NEW', 7, '2014-03-15 09:18:00'),
-(8, 1, 'suppliers', 'DEL', 7, '2014-03-15 12:01:03'),
-(9, 1, 'suppliers', 'PUT', 6, '2014-03-15 12:09:26'),
-(10, 1, 'suppliers', 'NEW', 8, '2014-03-15 12:10:01'),
-(11, 1, 'suppliers', 'PUT', 8, '2014-03-15 12:10:07'),
-(12, 1, 'suppliers', 'DEL', 8, '2014-03-15 12:10:10'),
-(13, 1, 'items', 'PUT', 6, '2014-03-15 12:21:34'),
-(14, 1, 'items', 'PUT', 8, '2014-03-15 12:36:20'),
-(15, 1, 'items', 'PUT', 7, '2014-03-15 12:36:33'),
-(16, 1, 'items', 'NEW', 14, '2014-03-15 18:41:54'),
-(17, 1, 'items', 'PUT', 14, '2014-03-15 18:42:03'),
-(18, 1, 'items', 'DEL', 14, '2014-03-15 18:42:07'),
-(19, 1, 'checks', 'NEW', 1, '2014-03-16 01:31:28');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='日志表';
 
 -- --------------------------------------------------------
 
@@ -221,38 +148,11 @@ INSERT INTO `logs` (`lID`, `uid`, `table`, `op`, `opID`, `time`) VALUES
 -- 表的结构 `members`
 --
 
-CREATE TABLE IF NOT EXISTS `members` (
+CREATE TABLE `members` (
   `mID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(10) NOT NULL COMMENT '成员姓名',
   PRIMARY KEY (`mID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='成员表' AUTO_INCREMENT=14 ;
-
---
--- 转存表中的数据 `members`
---
-
-INSERT INTO `members` (`mID`, `name`) VALUES
-(1, '管理员'),
-(2, '访客'),
-(3, '工程师'),
-(4, '赵国维'),
-(5, '高振'),
-(11, '工程师2'),
-(12, '王黎晨'),
-(13, '赵昕');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `operate`
---
-
-CREATE TABLE IF NOT EXISTS `operate` (
-  `oID` varchar(3) NOT NULL COMMENT '指令代码',
-  `command` varchar(10) NOT NULL COMMENT '指令名称',
-  `memo` varchar(45) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`oID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='出入库指令表';
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='成员表';
 
 -- --------------------------------------------------------
 
@@ -260,22 +160,14 @@ CREATE TABLE IF NOT EXISTS `operate` (
 -- 表的结构 `suppliers`
 --
 
-CREATE TABLE IF NOT EXISTS `suppliers` (
+CREATE TABLE `suppliers` (
   `sID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `fullname` varchar(30) NOT NULL COMMENT '供应商全称',
-  `name` varchar(10) DEFAULT NULL COMMENT '供应商简称',
+  `name` varchar(10) NOT NULL COMMENT '供应商简称',
+  `taxid` varchar(25) NOT NULL,
   `memo` varchar(100) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`sID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
---
--- 转存表中的数据 `suppliers`
---
-
-INSERT INTO `suppliers` (`sID`, `fullname`, `name`, `memo`) VALUES
-(1, '天津鸿阳嘉业科技有限公司', '鸿阳嘉业', '打印机耗材'),
-(5, '中国联合通信有限公司天津分公司', '天津联通', '光纤网络服务'),
-(6, '天津市仁和办公设备有限公司', '仁和办公', '日本京瓷天津总代理，复印机维修，耗材购买。');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -283,23 +175,14 @@ INSERT INTO `suppliers` (`sID`, `fullname`, `name`, `memo`) VALUES
 -- 表的结构 `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `uID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `uname` varchar(16) NOT NULL DEFAULT 'guest' COMMENT '登录名',
   `passwd` varchar(32) NOT NULL DEFAULT 'guest' COMMENT '登录密码',
   `mID` int(10) unsigned DEFAULT NULL COMMENT '用户姓名',
   `role` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '用户角色:',
   PRIMARY KEY (`uID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
---
--- 转存表中的数据 `users`
---
-
-INSERT INTO `users` (`uID`, `uname`, `passwd`, `mID`, `role`) VALUES
-(1, 'admin', 'admin', 1, 5),
-(2, 'guest', 'guest', 2, 0),
-(3, 'abc', 'abc', 3, 2);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
